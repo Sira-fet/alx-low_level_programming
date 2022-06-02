9d31d114c242526a9cfa309bae989168ae5097ea
Sira-fet <biniamfeleke10@gmail.com>
2022-06-02 13:29:05 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
+
+
 /**
  *
- *   *  * main - prints the size of various types
- *    *   *
- *     *    * Return: 0 if exited properly, non-zeron otherwise
- *      * */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
