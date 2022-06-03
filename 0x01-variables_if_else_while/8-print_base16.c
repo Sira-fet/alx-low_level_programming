d9b1bec3689a493777624d3589f1dd137f045815
Sira-fet <biniamfeleke10@gmail.com>
2022-06-03 14:53:46 -0700

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ int main(void)
 	char ch;
 
 	for (n = 48; n < 58; n++)
-						{
+	{
 		putchar(n);
 	}
-        for (ch = 'a'; ch <= 'f'; ch++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
