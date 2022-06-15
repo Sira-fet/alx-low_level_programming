40ffded7b9861588c7ce82c730d6a9f2072cdc47
Sira-fet <biniamfeleke10@gmail.com>
2022-06-15 11:49:35 -0700

    task8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,32 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - Entry point
- * ONE if, TWO loops only...
- * @n: input
- * Return: decrypted string
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
  */
-char *rot13(char *n)
+char *rot13(char *s)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (n[i] != '\0')
+	for (i = 0; *(s + i); i++)
 	{
-		for (x = 0; x <= 51; x++)
+		for (j = 0; j < 52; j++)
 		{
-			if (n[i] == toswap[x])
+			if (a[j] == *(s + i))
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				*(s + i) = b[j];
+				break;
 			}
-			rot_c = rot_c * -1;
 		}
-		i++;
 	}
-	return (n);
+	return (s);
 }
