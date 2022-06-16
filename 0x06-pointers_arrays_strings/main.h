5f31437fd8a43e30691c7e3fe16a2c19e5e51657
Sira-fet <biniamfeleke10@gmail.com>
2022-06-16 10:50:36 -0700

    task10

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef _main_H_
-#define _main_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -9,6 +8,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *_strcat(char *dest, char *src);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
